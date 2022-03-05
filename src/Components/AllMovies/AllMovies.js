d689f9f5d722e5aa823912df4ec5b7fdc1f0c36b
shakil89427 <shakilahmed89427@gmail.com>
2022-03-05 19:21:50 +0600

    rechecked

--- src/Components/AllMovies/AllMovies.js
+++ src/Components/AllMovies/AllMovies.js
@@ -3,6 +3,7 @@ import "./AllMovies.css";
 import useStore from "../../Store/useStore";
 import Loading from "../Loading/Loading";
 import { Link } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 
 const AllMovies = () => {
   const { movies, loading } = useStore();
@@ -13,30 +14,34 @@ const AllMovies = () => {
       ) : (
         <div>
           <h2 className="header">Book your desire Movie Ticket</h2>
-          <div className="movies-maindiv">
-            {movies.map((movie) => (
-              <div key={movie.show.id} className="movie-div">
-                <div className="allmovie-imgdiv">
-                  <img
-                    className="allmovie-img"
-                    src={movie.show.image.medium}
-                    alt="movie_logo"
-                  />
-                </div>
-                <h3 className="movies-name">{movie.show.name}</h3>
-                <div className="genres">
-                  {movie.show.genres.map((genre) => (
-                    <p key={genre}>{genre}</p>
-                  ))}
-                </div>
-                <p>Language : {movie.show.language}</p>
-                <p>Ratings : {movie.show.rating.average || "No Ratings"}</p>
-                <Link to={`moviedetails/${movie.show.id}`}>
-                  <button>Details</button>
-                </Link>
-              </div>
-            ))}
-          </div>
+          <Container>
+            <Row>
+              {movies.map((movie) => (
+                <Col md={4} lg={3} key={movie.show.id} className="g-2">
+                  <div className="movie-div m-2 p-2">
+                    <div className="allmovie-imgdiv">
+                      <img
+                        className="allmovie-img"
+                        src={movie.show.image.medium}
+                        alt="movie_logo"
+                      />
+                    </div>
+                    <h3 className="movies-name">{movie.show.name}</h3>
+                    <div className="genres">
+                      {movie.show.genres.map((genre) => (
+                        <p key={genre}>{genre}</p>
+                      ))}
+                    </div>
+                    <p>Language : {movie.show.language}</p>
+                    <p>Ratings : {movie.show.rating.average || "No Ratings"}</p>
+                    <Link to={`moviedetails/${movie.show.id}`}>
+                      <button>Details</button>
+                    </Link>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </Container>
         </div>
       )}
     </>
--- src/Components/MovieDetails/MovieDetails.js
+++ src/Components/MovieDetails/MovieDetails.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 import { File, Play, Star, Translate } from "react-bootstrap-icons";
 import Loading from "../Loading/Loading";
 import Modal from "../Modal/Modal";
+import { Col, Container, Row } from "react-bootstrap";
 
 const MovieDetails = () => {
   const { movies, loading } = useStore();
@@ -20,63 +21,69 @@ const MovieDetails = () => {
     setMovie(data?.show);
   }, [id, loading, movies]);
   return (
-    <>
+    <div className="main">
       {loading ? (
         <Loading />
       ) : (
-        <div className="detail-main">
-          <div className="movie-imagediv">
-            <img
-              className="movie-image"
-              src={movie?.image?.original || movie?.image?.medium}
-              alt=""
-            />
-          </div>
-          <div className="movie-info">
-            <h1>{movie?.name}</h1>
-            <div className="small-info">
-              <div className="item">
-                <Translate />
-                <p>{movie?.language}</p>
+        <Container className="py-4">
+          <Row>
+            <Col sm={12} md={4} lg={4}>
+              <div className="p-3">
+                <img
+                  className="movie-image"
+                  src={movie?.image?.original || movie?.image?.medium}
+                  alt=""
+                />
               </div>
-              <div className="item">
-                <Star />
-                <p>{movie?.rating?.average || "No Rating"}</p>
+            </Col>
+            <Col sm={12} md={8} lg={8} className="d-flex align-items-center">
+              <div className="mt-4">
+                <h1>{movie?.name}</h1>
+                <div className="small-info">
+                  <div className="item">
+                    <Translate />
+                    <p>{movie?.language}</p>
+                  </div>
+                  <div className="item">
+                    <Star />
+                    <p>{movie?.rating?.average || "No Rating"}</p>
+                  </div>
+                  <div className="item">
+                    <File />
+                    <p>{movie?.type}</p>
+                  </div>
+                  <div className="item">
+                    <Play />
+                    <p>{movie?.status}</p>
+                  </div>
+                </div>
+                <p className="summary">{movie?.summary}</p>
+                <p>
+                  More info at
+                  <a
+                    className="more-info"
+                    href={movie?.officialSite || "https://google.com"}
+                    target="blank"
+                  >
+                    {movie?.officialSite || "https://google.com"}
+                  </a>
+                </p>
+                <h4>Last Premired</h4>
+                <p>{movie?.premiered}</p>
+                <h4>Next Show</h4>
+                <p>
+                  {movie?.schedule?.days[0]} at {movie?.schedule?.time}
+                </p>
+                <button onClick={() => setShow(true)} className="book">
+                  Book Now
+                </button>
+                {show && <Modal setShow={setShow} movie={movie} />}
               </div>
-              <div className="item">
-                <File />
-                <p>{movie?.type}</p>
-              </div>
-              <div className="item">
-                <Play />
-                <p>{movie?.status}</p>
-              </div>
-            </div>
-            <p className="summary">{movie?.summary}</p>
-            <p>
-              More info at
-              <a
-                className="more-info"
-                href={movie?.officialSite || "https://google.com"}
-                target="blank"
-              >
-                {movie?.officialSite || "https://google.com"}
-              </a>
-            </p>
-            <h4>Last Premired</h4>
-            <p>{movie?.premiered}</p>
-            <h4>Next Show</h4>
-            <p>
-              {movie?.schedule?.days[0]} at {movie?.schedule?.time}
-            </p>
-            <button onClick={() => setShow(true)} className="book">
-              Book Now
-            </button>
-            {show && <Modal setShow={setShow} movie={movie} />}
-          </div>
-        </div>
+            </Col>
+          </Row>
+        </Container>
       )}
-    </>
+    </div>
   );
 };
 
